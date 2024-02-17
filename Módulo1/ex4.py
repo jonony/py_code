42abc5bba338ec60bc3026117fa4d578843c09e5
Johny Carvalho <134290213+jonony@users.noreply.github.com>
2024-02-17 19:06:42 -0300

    Variáveis

--- Módulo1/ex4.py
+++ Módulo1/ex4.py
@@ -3,3 +3,11 @@ idade = 22
 
 print('Nome:', nome , '/', 'Idade:', idade)
 
+
+'''
+As váriaveis por si só nãao necessitam de definir como no caso do JS que seria
+var, let e const 
+Como python nõ é tipado ela pode ser definida a partir do nome desejado que queira ser chamada a função.
+
+'''
+
