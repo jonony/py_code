43de377be1128cdae5d3097f8d7c5362bc43fc54
Johny Carvalho <134290213+jonony@users.noreply.github.com>
2024-02-17 13:46:27 -0300

    Operadores matemáticos

---
+++ Módulo1/ex2.py
@@ -0,0 +1,19 @@
+print(4 -  5)
+print(4 *  5)
+print(4 /  5)
+print(4 // 5)
+print(4 %  5)
+print(4 ** 5)
+
+print('------------')
+
+#Exercício de fixação:
+
+#a
+print(2+3*3)
+
+#b
+print(4**2/3)
+
+#c
+print((9**2+2)*6-1)
