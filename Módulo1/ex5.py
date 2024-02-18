d2ead2464e7aada6be1893b1fe4062cd8a937f2e
Johny Carvalho <134290213+jonony@users.noreply.github.com>
2024-02-18 20:18:45 -0300

    Variáveis Lógicas

---
+++ Módulo1/ex5.py
@@ -0,0 +1,20 @@
+'''
+== Igualdade
+>  Maior que
+<  Menor que
+>= Maior ou Igual que
+<= Menor ou igual que
+!= Diferente
+
+'''
+
+a = 6 
+b = 8
+
+resposta = a <= b 
+print(resposta)
+
+# True
+
+nota = 8.1
+aprovado = nota >= 7.1
