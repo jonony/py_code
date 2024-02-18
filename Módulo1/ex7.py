7664cb15c47bb6cb7183997201a82efa52de0b06
Johny Carvalho <134290213+jonony@users.noreply.github.com>
2024-02-18 20:55:03 -0300

    Concatenação de Strings

---
+++ Módulo1/ex7.py
@@ -0,0 +1,12 @@
+nome = 'Johny Alaf'
+nome = nome + ' Alvarenga Carvalho'
+print(nome)
+
+
+ads = 'Suporte' + '-' * 2 + 'Técnico' + ' | '  + 'Club' + '-' + 'Petro'
+print(ads)
+
+
+lg = 'Linguagens de programações: \n' + '\t Python' + 5 * '-' + 'C' + 5 * '-' + 'JAVA' + 5 * '-' + 'PHP'
+print(lg)
+
