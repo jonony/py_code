b00c85f4d129c8ba86c992899ee0ad27c3c9c996
Johny Carvalho <134290213+jonony@users.noreply.github.com>
2024-02-19 13:35:15 -0300

    Create execício1.1.py

---
+++ Módulo1/execício1.1.py
@@ -0,0 +1,12 @@
+#
+# ? Exercício 1
+# * Desenvolva um algoritmo que solicite ao usuário dois números inteiros
+# * Imprima a soma destes números na tela
+#
+
+x = int(input('Digite um número inteiro: '))
+y = int(input('Digite outro número inteiro: '))
+
+s = x + y
+print(f'A soma entre {x} e {y} é {s}')
+
