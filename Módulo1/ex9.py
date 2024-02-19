c2184b235ab2960be586c592816eca3e30163758
Johny Carvalho <134290213+jonony@users.noreply.github.com>
2024-02-19 09:42:52 -0300

    Fatiamento de Strings

---
+++ Módulo1/ex9.py
@@ -0,0 +1,8 @@
+nome_filme = 'Harry Potter'
+print(nome_filme[5:12])
+
+
+# Podemos selecionar apenas uma parte da String utilizando [0:4]
+# Onde definimos o intervalo de inicio e o fim que será "recortado"
+# Podemos também definir apenas o fim, até onde vai ser "recortado" usando [:6]
+
