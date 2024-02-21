ab19355c6380f2bb5e57ebe98f74eee376e4ede2
Johny Carvalho <134290213+jonony@users.noreply.github.com>
2024-02-21 10:35:54 -0300

    if ( ):

---
+++ Módulo3/ex1.py
@@ -0,0 +1,7 @@
+x = float(input('Digite aqui um número: '))
+y = float(input('Digite aqui outro número: '))
+
+if(x > y):
+    print('O primeiro número é maior que o segundo')
+if(x < y):
+    print('O segundo número é maior que o primeiro')
