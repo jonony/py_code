35cf40fb0341e6b415e170c1eff0b119a9980a32
Johny Carvalho <134290213+jonony@users.noreply.github.com>
2024-02-23 10:02:41 -0300

    Número no intervalo

---
+++ Módulo3/ex8.py
@@ -0,0 +1,11 @@
+#
+#? Exercicio 4
+#* Escreva um algoritmo que lê um valor inteiro qualquer.
+#* Após verifique se este valor está contido dentro dos seguintes intervalos: -100 < x < -1 ou 1 < x < 100.
+#* Imprima na tela caso ele esteja em um dos intervalos.
+#
+
+x = int(input('Digite aqui um número inteiro: '))
+
+if((x > 1) and (x < 100)) or ((x < -1) and (x > -100)):
+    print('Um dos critérios foi atingido')
