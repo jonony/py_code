41882e1ff4b99388b9732336f79523be8303a7bb
Johny Carvalho <134290213+jonony@users.noreply.github.com>
2024-02-27 17:54:02 -0300

    Alteração para elif

--- Módulo3/ex10.py
+++ Módulo3/ex10.py
@@ -8,13 +8,11 @@ qtd     = int(input('Quantas unidades deseja comprar? '))
 if(produto == 1):
     pagar = qtd * 2.30
     print(f'Você selecionou {qtd} maçãs e irá pagar R${pagar:.2f}')
-else:
-    if(produto == 2):
+elif(produto == 2):
         pagar = qtd * 1.75
         print(f'Você selecionou {qtd} laranjas e irá pagar R${pagar:.2f}')
-    else:
-         if(produto == 3):
-            pagar = qtd * 2.70
-            print(f'Você selecionou {qtd} melancias e irá pagar R${pagar:.2f}')
-         else:
-             print('Produto inexistente')
+elif(produto == 3):
+        pagar = qtd * 2.70
+        print(f'Você selecionou {qtd} melancias e irá pagar R${pagar:.2f}')
+else:
+    print('Produto inexistente')
